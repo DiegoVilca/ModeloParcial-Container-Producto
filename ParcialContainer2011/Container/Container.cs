4acf0a57623216e77c9f35c0643a519303b46ef4
diegoVilca <diegovilca@gmail.com>
2016-09-21 03:42:03 -0300

    Parcial resuelto subido a GitHub.

---
+++ ParcialContainer2011/Container/Container.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container
+{
+    public class Container
+    {
+        private int _capacidad;
+        private string _empresa;
+        private List<Producto> _listaProductos;
+
+
+        //Constructor
+
+        public Container(int capacidad, string Empresa)
+        {
+            this._listaProductos = new List<Producto>();
+            this._capacidad = capacidad;
+            this._empresa = Empresa;
+        }
+
+
+        #region Metodos
+
+        public bool Agregar(Producto proUno)
+        {
+            foreach (Producto item in this._listaProductos)
+            {
+                if (item == proUno)
+                    return false;
+                
+            }
+
+            if (this._capacidad > this._listaProductos.Count)
+            {
+                this._listaProductos.Add(proUno);
+                return true;
+            }
+
+            return false;
+
+
+        }
+
+        public static void Mostrar(Container Contenedor)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("-__-__Container__-__-");
+            sb.AppendLine("Empresa: " + Contenedor._empresa);
+            sb.AppendLine("Capacidad: " + Contenedor._capacidad);
+            sb.Append("Productos:");
+            
+            Console.WriteLine(sb.ToString());
+
+            foreach (Producto item in Contenedor._listaProductos)
+            {
+                item.mostrar();
+            }
+
+        }
+
+        
+
+        #endregion Metodos
+
+
+
+
+        #region Sobrecargas
+
+        public static List<Producto> operator -(Container contenedorUno, eTipoComestible tipo)
+        {
+            List<Producto> miLista = new List<Producto>();
+
+            foreach (Producto item in contenedorUno._listaProductos)
+            {
+                if(item == tipo)
+                    miLista.Add(item);
+            }
+
+            return miLista;
+        }
+
+
+        public static bool operator ==(Container contenedor, Producto proUno)
+        {
+            foreach (Producto item in contenedor._listaProductos)
+            {
+                if (item == proUno)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(Container contenedor, Producto proUno)
+        {
+            return !(contenedor == proUno);
+        }
+
+
+        #endregion Sobrecargas
+
+    }
+}
---
+++ ParcialContainer2011/Container/Product.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container
+{
+    public class Producto
+    {
+        private int _codigoDeBarra;
+        private string _nombre;
+        private double _precio;
+        private eTipoComestible _tipo;
+
+        #region Constructores
+
+
+        private Producto(int codigoDeBarra)
+        {
+            this._codigoDeBarra = codigoDeBarra;
+        }
+
+        public Producto(int codigoDeBarra, string nombre, eTipoComestible tipo) :this(codigoDeBarra)
+        {
+            this._nombre = nombre;
+            this._tipo = tipo;
+        }
+
+        public Producto(int codigoDeBarra, string nombre, eTipoComestible tipo, double precio) :this(codigoDeBarra,nombre,tipo)
+        {
+            this._precio = precio;
+        }
+
+        #endregion Constructores
+
+
+        //Metodos
+        public void mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendLine("Nombre: " + this._nombre);
+            sb.AppendLine("Tipo: " + this._tipo);
+            sb.AppendLine("Codigo: " + this._codigoDeBarra);
+            sb.Append("Precio: " + this._precio);
+            
+            Console.WriteLine(sb.ToString());
+        }
+
+
+        #region Sobrecargas
+
+        public static bool operator ==(Producto proUno, eTipoComestible tipo)
+        {
+            if (proUno._tipo == tipo)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Producto proUno, eTipoComestible tipo)
+        { 
+            return !(proUno == tipo);
+        }
+
+
+        public static bool operator ==(Producto proUno, Producto proDos)
+        {
+            if (proUno._codigoDeBarra == proDos._codigoDeBarra)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Producto proUno, Producto proDos)
+        {
+            return !(proUno == proDos);
+        }
+
+
+        #endregion Sobrecargas
+
+        /*
+         * Temas parcial
+         * 
+         * creacion constructor, constructor correcto, que es constructor correcto
+         * sobrecarga operadores
+         * sobrecargar un operador implicito clase ppuerta que me lo transforme a string
+         * arrays, como crearlo y como recorrerlo
+         * metodo de instancia, metodo estatico
+         * utilizacion del this
+         * constructores estaticos
+         * propiedades de lectura, escritura
+         * (conceptualmente) diferencias o igualdades entre por ejemplo, met estatico y de instancia
+         * similitudes: estan en la misma clase, reciben parametros
+         * diferencias: a uno se lo llama con la Clase. al otro hay que instanciarlo
+         * dif/igual clase-objeto, propidad-atributo, constructor-metodo
+         * errores de sintaxis
+         * Que muestra un codigo
+         * 
+         * Alumno A = new alumno();
+         * A.nombre = "Jose";
+         * Alumno B= (string) "pepe";
+         * Alumno C = new alumno();
+         * c.nombre = "Maria";
+         * Alumno d = new Allumno()
+         * 
+         * b=a
+         * d=b
+         * 
+         * a.nombre = "natalia";
+         * //ver referencia, copias
+         * console.(d.nombre)
+         * 
+         * ///////////////////////////////////
+         * 
+         * Siete puntos
+         * 
+         * 
+         * 
+         */
+
+    }
+}
